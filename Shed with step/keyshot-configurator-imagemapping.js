9b8b5ec4931ddd7eb1f68fcffa0040a01b11b2c7
wmr-zac <95313351+wmr-zac@users.noreply.github.com>
2021-11-30 16:09:18 -0400

    First render test

---
+++ Shed with step/keyshot-configurator-imagemapping.js
@@ -0,0 +1,57 @@
+imagemappingdata =
+[
+    {
+        "image": "Shed with step.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "4+37"
+        ]
+    },
+    {
+        "image": "Shed with step.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "4+43"
+        ]
+    },
+    {
+        "image": "Shed with step.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "4+50"
+        ]
+    },
+    {
+        "image": "Shed with step.4.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "4+64"
+        ]
+    },
+    {
+        "image": "Shed with step.5.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "4+79"
+        ]
+    },
+    {
+        "image": "Shed with step.6.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "4+93"
+        ]
+    }
+];
---
+++ Shed with step/keyshot-configurator-modeldata.js
@@ -0,0 +1,74 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "name": "Material set 1 1"
+                }
+            ],
+            "name": "Material set 1"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "ash%20grey.png",
+                    "multimaterials": [
+                        "4+37"
+                    ],
+                    "name": "Ash Grey"
+                },
+                {
+                    "icon": "avocado.png",
+                    "multimaterials": [
+                        "4+43"
+                    ],
+                    "name": "Avocado"
+                },
+                {
+                    "checked": true,
+                    "icon": "beige.png",
+                    "multimaterials": [
+                        "4+50"
+                    ],
+                    "name": "Beige"
+                },
+                {
+                    "icon": "black.png",
+                    "multimaterials": [
+                        "4+64"
+                    ],
+                    "name": "Black"
+                },
+                {
+                    "icon": "bone%20white.png",
+                    "multimaterials": [
+                        "4+79"
+                    ],
+                    "name": "Bone White"
+                },
+                {
+                    "icon": "bronze.png",
+                    "multimaterials": [
+                        "4+93"
+                    ],
+                    "name": "Bronze"
+                }
+            ],
+            "name": "Paint Textured Black"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "materialgroups": [
+                "Material set 1",
+                "Paint Textured Black"
+            ],
+            "modelset": 1005,
+            "name": "Inset Shed"
+        }
+    ],
+    "studios": []
+};
